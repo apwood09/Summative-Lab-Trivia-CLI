32206825347bc6d107eb2dabe8d0a067dac2565d
abbi portwood <apwood09@gmail.com>
2026-03-12 16:09:19 -0400

    contains questions.js

---
+++ data/questions.js
@@ -0,0 +1,50 @@
+export const questions = [
+    // question 1 
+    {
+    type: 'list', 
+    name: 'q1', 
+    message: 'Which 1990s group is the best-selling girl group of all time?',  
+    choices: ['Destineys Child', 'TLC', 'Salt-N-Pepper', 'Spice Girls'], 
+    correct: 'Spice Girls', 
+    },
+    // question 2 
+    {
+    type: 'list', 
+    name: 'q2', 
+    message: 'What was the best-selling video game console of the 1990s?',  
+    choices: ['Sega Genesis', 'Sony PlayStation', 'Super Nintendo', 'Game Cube'], 
+    correct: 'Sony PlayStation', 
+    },
+    // question 3 
+    {
+    type: 'list', 
+    name: 'q3', 
+    message: 'What was the name of the social media site that was the most popular in the world from 2005 to 2008?',  
+    choices: ['Myspace', 'Facebook', 'Instagram', 'Snapchat'], 
+    correct: 'Myspace', 
+    },
+    // question 4
+     {
+    type: 'list', 
+    name: 'q4', 
+    message: 'Which 2000s movie features the quote, "On Wednesdays, we wear pink"?',  
+    choices: ['13 Going on 30', 'Mean Girls', 'Legaly Blond', 'Bring It On'], 
+    correct: 'Mean Girls', 
+    },
+    // question 5 
+     {
+    type: 'list', 
+    name: 'q5', 
+    message: 'Which 2000s pop star sang the hit "Toxic"',  
+    choices: ['Jessica Simpson', 'Britney Spears', 'Christina Augilera', 'Pink'], 
+    correct: 'Britney Spears', 
+    },
+    // question 6 
+     {
+    type: 'list', 
+    name: 'q6', 
+    message: 'Who sang the 1992 hit "I Will Always Love You"?',  
+    choices: ['Tina Turner', 'Mariah Carey', 'Whitney Houston', 'Janet Jackson'], 
+    correct: 'Whitney Houston', 
+    },
+]; 
